cbca3a01416e50c2c2f069f1a6ecc43359f59bab
David McCullough <dmccullough488@gmail.com>
2025-03-16 02:29:22 -0500

    Added function that returns an [] of 10 random numbers between 1-6

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
 import React from "react"
 import Die from "./Die";
 
+
 export default function App() {
 
-  const diceValues = Array(10).fill(1);
+  function generateAllNewDice() {
+    return new Array(10)
+      .fill(0)
+      .map(() => Math.ceil(Math.random() * 6))
+  }
+
+  console.log(generateAllNewDice());
+
 
   return (
     <main>
       <div className="dice-container">
         <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
         <Die value={1} />
         <Die value={1} />
         <Die value={1} />
         <Die value={1} />
       </div>
     </main>
-  );
+  )
+
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,3 +1,5 @@
+import React from "react"
+
 export default function Die(props) {
   return (
     <button>{props.value}</button>
