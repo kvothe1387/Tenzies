18072233a4b9a9ad383d289827405a6fd81fe83e
David McCullough <dmccullough488@gmail.com>
2025-03-16 01:48:00 -0500

    added die component & styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import React from "react"
+import Die from "./Die";
+
 export default function App() {
+
+  const diceValues = Array(10).fill(1);
+
   return (
     <main>
-
+      <div className="dice-container">
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
     </main>
-  )
+  );
 }
