d6483537d6dab4e6b80aa0c83dca34464def72b1
David McCullough <dmccullough488@gmail.com>
2025-03-21 01:25:09 -0500

    *

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/Tenzies/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  }
+  plugins: [react()],
 })
