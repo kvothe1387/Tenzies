ee0b285bbb579c659959b897a7e9e75a66f73e21
David McCullough <dmccullough488@gmail.com>
2025-03-20 23:32:20 -0500

    added confetti package to make confetti drop when game is won.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Die from "./Die";
 import { nanoid } from "nanoid";
+import Confetti from 'react-confetti'
 
 export default function App() {
 
@@ -17,10 +18,14 @@ export default function App() {
   const [dice, setDice] = React.useState(generateAllNewDice());
 
   function rollDice() {
-    setDice(oldDice => oldDice.map(die => {
-      return die.isHeld ? die :
-        { ...die, value: Math.ceil(Math.random() * 6) }
-    }))
+    if (gameWon) {
+      setDice(generateAllNewDice())
+    } else {
+      setDice(oldDice => oldDice.map(die => {
+        return die.isHeld ? die :
+          { ...die, value: Math.ceil(Math.random() * 6) }
+      }))
+    }
   };
 
   function hold(id) {
@@ -49,6 +54,7 @@ export default function App() {
 
   return (
     <main>
+      {gameWon && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
