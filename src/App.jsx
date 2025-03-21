3fd14c4bba3c1a877c3a36907dd46bc87d205e4a
David McCullough <dmccullough488@gmail.com>
2025-03-21 00:13:54 -0500

    added fun to allow the user to play a new game when the button is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,14 @@
-import React from "react";
-import Die from "./Die";
-import { nanoid } from "nanoid";
-import Confetti from 'react-confetti'
+import { useState } from "react"
+import Die from "./Die"
+import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App() {
+  const [dice, setDice] = useState(() => generateAllNewDice());
 
-  function generateAllNewDice() {
-    return new Array(10)
-      .fill(0)
-      .map(() => ({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid()
-      }))
-  };
-
-  const [dice, setDice] = React.useState(generateAllNewDice());
-
-  function rollDice() {
-    if (gameWon) {
-      setDice(generateAllNewDice())
-    } else {
-      setDice(oldDice => oldDice.map(die => {
-        return die.isHeld ? die :
-          { ...die, value: Math.ceil(Math.random() * 6) }
-      }))
-    }
-  };
-
-  function hold(id) {
-    setDice(oldDice => oldDice.map(die => {
-      return die.id === id ?
-        { ...die, isHeld: !die.isHeld } : die
-    }))
-  };
-
-  const gameWon =
-    dice.every(die => die.isHeld) &&
+  // create the gameWon variable
+  const gameWon = dice.every(die => die.isHeld) &&
     dice.every(die => die.value === dice[0].value);
-
-  if (gameWon) {
-    console.log("Game won!")
-  }
 
   const diceElements = dice.map(dieObj => (
     <Die
@@ -52,6 +19,39 @@ export default function App() {
     />
   ));
 
+  function generateAllNewDice() {
+    console.log("generateAllNewDice was called!")
+    return new Array(10)
+      .fill(0)
+      .map(() => ({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+      }))
+  };
+
+  function rollDice() {
+    if (gameWon) {
+      // reset the game with all new dice when the user has won
+      setDice(generateAllNewDice())
+    } else {
+      // normal dice roll during gameplay
+      setDice(oldDice => oldDice.map(die =>
+        die.isHeld ?
+          die :
+          { ...die, value: Math.ceil(Math.random() * 6) }
+      ))
+    }
+  };
+
+  function hold(id) {
+    setDice(oldDice => oldDice.map(die =>
+      die.id === id ?
+        { ...die, isHeld: !die.isHeld } :
+        die
+    ))
+  }
+
   return (
     <main>
       {gameWon && <Confetti />}
@@ -60,11 +60,9 @@ export default function App() {
       <div className="dice-container">
         {diceElements}
       </div>
-      <button className="roll-dice" onClick={rollDice}
-      >
+      <button className="roll-dice" onClick={rollDice}>
         {gameWon ? "New Game" : "Roll"}
       </button>
     </main>
-  );
-
+  )
 }
