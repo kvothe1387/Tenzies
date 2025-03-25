24e43bbe0a8f667e2c84e82569c608eafd4d8972
David McCullough <dmccullough488@gmail.com>
2025-03-24 21:41:04 -0500

    added accessiblitily for screen readers

--- src/Die.jsx
+++ src/Die.jsx
@@ -10,6 +10,7 @@ export default function Die(props) {
     <button
       style={styles}
       onClick={props.hold}
+      aria-pressed={props.isHeld}
       aria-label={`Die with value ${props.value}, 
         ${props.isHeld ? "held" : "not held"}`}
     >{props.value}</button>
